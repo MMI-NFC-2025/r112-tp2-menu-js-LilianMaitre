249144031e0d1de9f25ade7a2fc731e9a18420be
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-13 14:59:08 +0100

    noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 const toggle = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav.menu');
 
-
 toggle.addEventListener('click', () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
     console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.setAttribute('aria-hidden', isOpen);
     toggle.setAttribute('aria-expanded', isClosed);
-}); 
+    document.body.classList.toggle('noscroll', isOpen);
+});
