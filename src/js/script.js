04b4a1a07a410260aa7df426867b12aa3792b6db
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-13 14:29:30 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@ const nav = document.querySelector('nav.menu');
 
 
 toggle.addEventListener('click', () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.setAttribute('aria-hidden', isOpen);
     toggle.setAttribute('aria-expanded', isClosed);
 }); 
